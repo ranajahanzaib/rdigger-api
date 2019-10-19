0767c5ba68ce2b0979ff869c7c3e8dffc3540daa
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-19 16:08:41 +0500

    .

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
+	firebase "firebase.google.com/go"
+	"github.com/gorilla/mux"
 	nspx "github.com/nspx/core"
+	"google.golang.org/appengine"
 )
+
+// Product type
+type Product struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Init Books var as a slice Book struct
+var products []Product
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
 
 func main() {
 	// Init message
@@ -12,4 +33,44 @@ func main() {
 
 	// See if the nspx is imported.
 	nspx.Hello()
+
+	// Init Firestore
+
+	// Use the application default credentials
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: "rdiggerapi"}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	// Store sample data
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+
+	// Start router
+	r := mux.NewRouter()
+
+	// Sample Data
+	products = append(products, Product{ID: "1", Name: "HP 840 G3 Laptop"})
+
+	// Endpoints
+	r.HandleFunc("/products", getProducts).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+	appengine.Main()
+
 }
